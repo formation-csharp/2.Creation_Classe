e455ac271d5f58ab7c27ab6c4518972e495e01e3
essarraj.fouad <essarraj.fouad@gmail.com>
2019-12-18 12:24:17 +0100

    Création de la classe "Projet"

---
+++ Creation_Classe/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creation_Classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Création d'un objet de type Projet 
+            Projet projet1 = null;
+
+            // Création de l'instance Projet
+            projet1 = new Projet("Projet 1");
+
+            // Utilisation de la propriété "Titre
+            projet1.Titre = "Création d'une application";
+
+            // Affichage de l'objet
+            Console.WriteLine(projet1);
+            Console.WriteLine(projet1.ToString());
+            Console.WriteLine(projet1.Titre);
+
+            // equals
+            Projet projet2 = new Projet();
+            projet2.Titre = "Création d'une application";
+
+            if(projet1.Equals(projet2))
+            {
+                Console.WriteLine("Projet 1 égale Projet 2");
+            }
+            else
+            {
+                Console.WriteLine("Projet 1 n'égale pas Projet 2");
+            }
+
+           
+          
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Creation_Classe/Projet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creation_Classe
+{
+    class Projet
+    {
+        // Variables - Attributs
+        private string _Titre;
+        private string _Description;
+        public Projet(string Titre)
+        {
+            this.Titre = Titre;
+        }
+        // Constructeur par défaut
+        public Projet() {}
+
+        // Propriété
+        public string Titre
+        {
+            set
+            {
+                _Titre = value;
+            }
+            get
+            {
+                return _Titre;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return "Projet : " + this._Titre;
+        }
+
+        public override bool Equals(object obj)
+        {
+           
+            Projet projet2 = obj as Projet;
+            if(projet2 == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
